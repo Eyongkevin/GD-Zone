536a6e882d9143b84a82c4eff04859046a20c198
Eyong Kevin Enowanyo <tonyparkerkenz@gmail.com>
2022-10-19 21:34:44 +0100

    style frontend inputs

--- apps/workspace/forms.py
+++ apps/workspace/forms.py
@@ -3,11 +3,15 @@ from django import forms
 
 class AccumulativePercentageForm(forms.Form):
     amount = forms.IntegerField(
-        widget=forms.NumberInput(attrs={"class": "amount-size"}), label="B"
+        widget=forms.NumberInput(attrs={"class": "amount-size"}), label="B", min_value=1
     )
     percentage = forms.IntegerField(
-        widget=forms.NumberInput(attrs={"class": "percentage-size"}), label=" %"
+        widget=forms.NumberInput(attrs={"class": "percentage-size"}),
+        label=" %",
+        min_value=1,
     )
     period = forms.IntegerField(
-        widget=forms.NumberInput(attrs={"class": "period-size"}), label=" P"
+        widget=forms.NumberInput(attrs={"class": "period-size"}),
+        label=" P",
+        min_value=1,
     )
